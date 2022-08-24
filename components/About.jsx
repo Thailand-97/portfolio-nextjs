2a00e0a3746ef99cc82f13ef339809d97f00e2db
thailand97 <75339681+thailand97@users.noreply.github.com>
2022-08-23 22:38:04 -0400

    fourth commit

--- components/About.jsx
+++ components/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import AboutImg from '../public/assets/skills/about.jpg';
 
 const About = () => {
   return (
@@ -30,7 +31,7 @@ const About = () => {
           </Link>
         </div>
         <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-          <Image src='/../public/assets/skills/about.jpg' height='450' width='400' className='rounded-xl' alt='/' />
+          <Image src={AboutImg} className='rounded-xl' alt='/' /> 
         </div>
       </div>
     </div>
